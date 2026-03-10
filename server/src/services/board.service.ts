026964372e859b92f55614c8db4e1449e8f2c4f1
laser <sachin06kaizen@gmail.com>
2026-03-10 20:55:13 +0530

    feat:create service for task and board

---
+++ server/src/services/board.service.ts
@@ -0,0 +1,36 @@
+import Board from '../models/Board';
+import { CreateBoardPayload } from '../schemas/board.schema';
+
+class BoardService {
+  async createBoard(userId: string, data: CreateBoardPayload) {
+    return await Board.create({
+      ...data,
+      user: userId,
+      tasks: []
+    });
+  }
+
+  async getAllBoards(userId: string) {
+    return await Board.find({ user: userId })
+      .populate('tasks') // Populates the task array with actual task documents
+      .sort({ createdAt: -1 });
+  }
+
+  async getBoardById(boardId: string, userId: string) {
+    return await Board.findOne({ _id: boardId, user: userId }).populate('tasks');
+  }
+
+  async deleteBoard(boardId: true, userId: string) {
+    return await Board.findOneAndDelete({ _id: boardId, user: userId });
+  }
+
+  async updateBoardTitle(boardId: string, userId: string, title: string) {
+    return await Board.findOneAndUpdate(
+      { _id: boardId, user: userId },
+      { title, updatedAt: new Date() },
+      { new: true }
+    );
+  }
+}
+
+export default new BoardService();
---
+++ server/src/services/task.service.ts
@@ -0,0 +1,42 @@
+import Task from '../models/Task';
+import Board from '../models/Board';
+import { CreateTaskPayload } from '../schemas/task.schema';
+
+class TaskService {
+  async createTask(userId: string, boardId: string, data: CreateTaskPayload) {
+    const task = await Task.create({
+      ...data,
+      user: userId,
+    });
+
+    // Automatically push the new task ID to the specified board
+    await Board.findByIdAndUpdate(boardId, {
+      $push: { tasks: task._id }
+    });
+
+    return task;
+  }
+
+  async getTasksByUser(userId: string) {
+    return await Task.find({ user: userId }).sort({ createdAt: -1 });
+  }
+
+  async updateTask(taskId: string, userId: string, updateData: Partial<CreateTaskPayload>) {
+    return await Task.findOneAndUpdate(
+      { _id: taskId, user: userId },
+      { ...updateData, updatedAt: new Date() },
+      { new: true }
+    );
+  }
+
+  async deleteTask(taskId: string, userId: string) {
+    const task = await Task.findOneAndDelete({ _id: taskId, user: userId });
+    if (task) {
+      // Clean up reference in any board
+      await Board.updateMany({}, { $pull: { tasks: taskId } });
+    }
+    return task;
+  }
+}
+
+export default new TaskService();
